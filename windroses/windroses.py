96a282ad4f388b5a38eb9c3472e3de80f40b0c9b
Marcia Marques <marcia.marques@alumni.usp.br>
2021-08-11 13:22:26 -0300

    Add wind rose scatter and wind rose scatter season functions

--- windroses/windroses.py
+++ windroses/windroses.py
@@ -134,6 +134,124 @@ def wind_rose_season(df, wd, nbins=16, xticks=8, wind=True, south=True, ylim=Fal
     return
 
 
+def wind_rose_scatter(df, ws, wd, xticks=8, size=2, plot=111, wind=True, ylim=False, yaxis=False, yticks=False):
+    """
+    Return a scatter polar plot of wind speed.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The pandas DataFrame holding the data.
+    ws : str
+        Wind speed column name.
+    wd : str
+        Wind direction column name.
+    xticks : int {4, 8, 16} , optional
+        Number of xticks, default is 8.
+    size : int or float, default os 2
+        Size of scatter points.
+    plot : int, optional
+        nrows, ncols, index to define subplots, default is 111,
+        it is used to plot seasonal wind roses.
+    wind : bool, optional
+        Show cardinal directions (i.e. ['N', 'NE', ...]), defaults is True.
+    ylim : int or float, optional
+        Maximum limit for y-axis, default is False.
+    yaxis : int or flot, optional
+        Position of y-axis (0 - 360), default is False.
+    yticks : list-like, optional
+        List of yticks, default is False.
+    """
+
+    labels = ['N', 'NNE', 'NE', 'ENE', 'E', 'SSE', 'SE', 'SSE',
+              'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
+
+    # windrose
+    ax = plt.subplot(plot, projection='polar')
+    ax.scatter(x=[radians(x) for x in df[wd].values], y=df[ws].values, s=size)
+    ax.set_theta_zero_location('N')
+    ax.set_theta_direction(-1)
+    plt.grid(axis='both', which='major', linestyle='--')
+
+    # categorical xticklabels
+    if xticks == 4:
+        ax.set_xticks([radians(x) for x in np.arange(0, 360, 360 / xticks)])
+        if wind:
+            ax.set_xticklabels([x for x in labels[::4]])
+    elif xticks == 8:
+        ax.set_xticks([radians(x) for x in np.arange(0, 360, 360 / xticks)])
+        if wind:
+            ax.set_xticklabels([x for x in labels[::2]])
+    elif xticks == 16:
+        ax.set_xticks([radians(x) for x in np.arange(0, 360, 360 / xticks)])
+        if wind:
+            ax.set_xticklabels(labels)
+    else:
+        raise Exception("xticks should be 4, 8, or 16")
+
+    # y axis limit
+    if ylim:
+        plt.ylim(0, ylim)
+
+    # y axis position
+    if yaxis:
+        ax.set_rlabel_position(yaxis)
+
+    # y axis ticks
+    if yticks:
+        ax.set_yticks(yticks)
+
+    return
+
+
+def wind_rose_scatter_season(df, ws, wd, xticks=8, size=2, wind=True, south=True,
+                             ylim=False, yaxis=False, yticks=False):
+    """
+    Return a scatter polar plot of wind speed.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The pandas DataFrame holding the data.
+    ws : str
+        Wind speed column name.
+    wd : str
+        Wind direction column name.
+    xticks : int {4, 8, 16} , optional
+        Number of xticks, default is 8.
+    size : int or float, default os 2
+        Size of scatter points.
+    wind : bool, optional
+        Show cardinal directions (i.e. ['N', 'NE', ...]), defaults is True.
+    south : bool, optional, default is True
+        If True, seasons are calculated to Southern Hemisphere, otherwise Northern Hemisphere.
+    ylim : int or float, optional
+        Maximum limit for y-axis, default is False.
+    yaxis : int or flot, optional
+        Position of y-axis (0 - 360), default is False.
+    yticks : list-like, optional
+        List of yticks, default is False.
+    """
+
+    # create a new column season
+    if south:
+        df['season'] = ((df.index.month % 12 + 3) // 3).map({1: 'Summer', 2: 'Autumn', 3: 'Winter', 4: 'Spring'})
+    else:
+        df['season'] = ((df.index.month % 12 + 3) // 3).map({1: 'Winter', 2: 'Spring', 3: 'Summer', 4: 'Autumn'})
+
+    # windroses
+    for i, season in enumerate(df['season'].unique()):
+        df_season = df.copy()
+        df_season = df_season.loc[df_season['season'] == season]
+        wind_rose_scatter(df_season, ws=ws, wd=wd, xticks=xticks, size=size, plot=221+i, wind=wind,
+                          ylim=ylim, yaxis=yaxis, yticks=yticks)
+        plt.title(season + '\n', fontsize=14, fontweight='bold')
+
+    plt.tight_layout()
+
+    return
+
+
 def wind_rose_speed(df, ws, wd, nbins=16, xticks=8, plot=111, wind=True, ylim=False, yaxis=False, yticks=False,
                     lims=False, loc='lower left'):
     """
